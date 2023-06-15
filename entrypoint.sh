31df80553e8ebbe916fba33f346d284da60b553b
MaximMatveevOneUp <129727979+MaximMatveevOneUp@users.noreply.github.com>
2023-06-15 14:33:03 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,15 @@ set -e
 
 mkdir -p ~/.aws
 touch ~/.aws/credentials
+touch ~/.aws/config
 
 echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+echo "[default]
+retry_mode = standard
+max_attempts = 3" > ~/.aws/config
 
 aws s3 cp ${FILE} s3://${AWS_S3_BUCKET}${AWS_S3_PATH} \
             --region ${AWS_REGION} $*
